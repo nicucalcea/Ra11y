c95d5f4376ac9c837cc043dc5e35c3e5aad329f5
Nicu Calcea <mail@nicu.md>
2023-09-22 17:52:10 +0100

    better colour extraction

--- R/test_plot.R
+++ R/test_plot.R
@@ -14,18 +14,21 @@ test_plot <- function(plot, test = c("cvd", "alt")) {
   if ("cvd" %in% test) {
     # start by extracting colours and fills of the plot
     plot_build <- ggplot2::ggplot_build(plot)
-    plot_colours <- unique(c(plot_build$data[[1]][["fill"]], plot_build$data[[1]][["colour"]], plot_build$data[[1]][["color"]]))
+    # plot_colours <- unique(c(plot_build$data[[1]][["fill"]], plot_build$data[[1]][["colour"]], plot_build$data[[1]][["color"]]))
+    plot_colours <- unique(unlist(lapply(plot_build$data, function(x) {try(c(x$colour, x$color, x$fill))})))
     plot_colours <- plot_colours[!is.na(plot_colours)]
 
     # test the colours for colour blindness
-    cvd_list <- test_colourblind(colours = plot_colours)
+    if (length(plot_colours) > 1) {
+      cvd_list <- test_colourblind(colours = plot_colours)
 
-    # display warning if something is wrong
-    if (length(cvd_list) > 0) {
-      cli::cli_h1("Colour blindness")
-      cli::cli_alert_danger("There may be issues with the following colour combinations:")
-      cli::cli_ul(lapply(cvd_list, paste0, collapse = " — "))
-      cli::cli_text("Run {.run colorblindr::cvd_grid()} for colour-deficiency simulations of your plot.")
+      # display warning if something is wrong
+      if (length(cvd_list) > 0) {
+        cli::cli_h1("Colour blindness")
+        cli::cli_alert_danger("There may be issues with the following colour combinations:")
+        cli::cli_ul(lapply(cvd_list, paste0, collapse = " — "))
+        cli::cli_text("Run {.run colorblindr::cvd_grid()} for colour-deficiency simulations of your plot.")
+      }
     }
   }
 
