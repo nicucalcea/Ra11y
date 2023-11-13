82c53fc88944203f1b8a2dfb7e2b8888412eca39
Nicu Calcea <mail@nicu.md>
2023-11-13 16:34:39 +0000

    save alt to clipboard

--- R/alt_text_check.R
+++ R/alt_text_check.R
@@ -3,7 +3,7 @@
 #' Pass a plot to check whether it has alt text
 #'
 #' @param plot_name The plot to check for alt text
-#' @param alt_text_loc Whether you want it displayed in the console, saved to file, or both
+#' @param alt_text_loc Whether you want it displayed in the console, file, clipboard, or a combination
 #' @param save_filepath If above set to file, where do you want to save it? #'
 #' @examples
 #' alt_text_check(dplyr::starwars |>
@@ -23,17 +23,15 @@
 #' @rdname alt_text
 #' @importFrom ggplot2 get_alt_text
 #' @importFrom cli cli_h1 cli_alert_danger cli_alert_info
+#' @importFrom clipr write_clip
 #' @importFrom tools file_ext
-alt_text_check <- function(plot_name, alt_text_loc = "console", save_filepath) {
+alt_text_check <- function(plot_name, alt_text_loc = c("console"), save_filepath) {
   alt_text <- ggplot2::get_alt_text(plot_name)
-  alt_text_set = alt_text != ""
-  console <- "console" %in% alt_text_loc
-  file <- "file" %in% alt_text_loc
 
   cli::cli_h1("Alt text")
 
   # If alt text hasn't been set
-  if (!alt_text_set) {
+  if (alt_text == "") {
 
     # Nudge to write some alt text
     cli::cli_alert_danger('Did you forget to include {.href [alt text](https://medium.com/nightingale/writing-alt-text-for-data-visualization-2a218ef43f81)}? You can do so in {.run labs(alt = "This is some text describing the chart.")}.')
@@ -41,18 +39,23 @@ alt_text_check <- function(plot_name, alt_text_loc = "console", save_filepath) {
     # cli::cli_alert(paste0("Here\'s something to get you started: ", cli::style_italic(auto_alt)))
     # If alt text has been set
   } else {
-    if (console) {
-      # Write out alt text to console if they've asked for it
+
+    # Write out alt text to console
+    if ("console" %in% alt_text_loc) {
       cli::cli_alert_info(alt_text)
+    }
+
+    # Save alt text to file
+    if ("file" %in% alt_text_loc) {
+      alt_text_file <- gsub(paste0(tools::file_ext(save_filepath), "$"), "txt", save_filepath)
+      writeLines(alt_text, alt_text_file)
+      cli::cli_alert_info(paste0("Alt text written out to: ", alt_text_file))
+    }
+
+    # Save alt text to clipboard
+    if ("clipboard" %in% alt_text_loc) {
+      clipr::write_clip(alt_text)
     }
   }
 
-  # Save alt text to file
-  if (file) {
-    alt_text_file <- gsub(paste0(tools::file_ext(save_filepath), "$"), "txt", save_filepath)
-    if (console) {
-      cli::cli_alert_info(paste0("Alt text written out to: ", alt_text_file))
-    }
-    writeLines(alt_text, alt_text_file)
-  }
 }
